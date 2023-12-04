80d19f2125735f49abb8c78220e1b4becc4b7409
Robert Grizzell <robert@grizzell.me>
2023-12-03 23:55:05 -0600

    Refactoring firmware for better maintainability

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,32 @@
 //  SPDX-FileCopyrightText: 2023 Robert Grizzell
 //  SPDX-License-Identifier: MIT
 #include <Arduino.h>
-#include <Queue.h>
 #include <Wire.h>
 
-#define I2C_ADDR 0x55
-#define SDA 2
-#define SCL 10
-#define LITE 9
-#define INT 46
+#include <queue>
 
-uint8_t rows[] = {0, 3, 19, 12, 18, 6, 7};
-const int rowCount = sizeof(rows) / sizeof(rows[0]);
-uint8_t cols[] = {1, 4, 5, 11, 13};
-const int colCount = sizeof(cols) / sizeof(cols[0]);
+#define I2C_ADDR 0x55
+#define SDA_PIN 2
+#define SCL_PIN 10
+#define LED_PIN 9
+#define INT_PIN 46
+
+using namespace std;
+
+void i2cReceive(int bytesRead);
+void i2cTransmit();
+void readKeyMatrix();
+void processKeyStates();
+bool keyReleased(int colIndex, int rowIndex);
+bool keyPressed(int colIndex, int rowIndex);
+bool isAssignedKey(int colIndex, int rowIndex);
+void addKeyEvent(char key);
+void setBacklight(bool state);
+
+uint8_t rowPins[] = {0, 3, 19, 12, 18, 6, 7};
+uint8_t colPins[] = {1, 4, 5, 11, 13};
+const int rowCount = sizeof(rowPins);
+const int colCount = sizeof(colPins);
 
 bool keys[colCount][rowCount];
 bool lastValue[colCount][rowCount];
@@ -21,25 +34,16 @@ bool changedValue[colCount][rowCount];
 char keyboard[colCount][rowCount];
 char keyboardSymbol[colCount][rowCount];
 
-bool symbolSelected;
 bool backlightState = false;
-Queue i2cBuffer;
-
-void onRequest();
-void onReceive(int bytesRead);
-void readMatrix();
-bool keyPressed(int colIndex, int rowIndex);
-bool keyActive(int colIndex, int rowIndex);
-bool isPrintableKey(int colIndex, int rowIndex);
-void printMatrix();
-void setBacklight(bool state);
+queue<char> eventBuffer;
+uint8_t eventsRequested = 1;
 
 void setup() {
   keyboard[0][0] = 'q';
   keyboard[0][1] = 'w';
   keyboard[0][2] = '\0';  // Symbol
   keyboard[0][3] = 'a';
-  keyboard[0][4] = '\0';  // At
+  keyboard[0][4] = '\0';  // Alt
   keyboard[0][5] = ' ';
   keyboard[0][6] = '\0';  // Mic
 
@@ -62,7 +66,7 @@ void setup() {
   keyboard[3][0] = 'u';
   keyboard[3][1] = 'h';
   keyboard[3][2] = 'y';
-  keyboard[3][3] = '\0';  // Enter
+  keyboard[3][3] = (char)0x0a;  // Enter
   keyboard[3][4] = 'b';
   keyboard[3][5] = 'n';
   keyboard[3][6] = 'j';
@@ -70,17 +74,17 @@ void setup() {
   keyboard[4][0] = 'o';
   keyboard[4][1] = 'l';
   keyboard[4][2] = 'i';
-  keyboard[4][3] = '\0';  // Backspace
+  keyboard[4][3] = (char)0x08;  // Backspace
   keyboard[4][4] = '$';
   keyboard[4][5] = 'm';
   keyboard[4][6] = 'k';
 
   keyboardSymbol[0][0] = '#';
   keyboardSymbol[0][1] = '1';
-  keyboardSymbol[0][2] = '\0';
+  keyboardSymbol[0][2] = '\0';  // Symbol
   keyboardSymbol[0][3] = '*';
-  keyboardSymbol[0][4] = '\0';
-  keyboardSymbol[0][5] = '\0';
+  keyboardSymbol[0][4] = '\0';  // Alt
+  keyboardSymbol[0][5] = ' ';
   keyboardSymbol[0][6] = '0';
 
   keyboardSymbol[1][0] = '2';
@@ -89,12 +93,12 @@ void setup() {
   keyboardSymbol[1][3] = '@';
   keyboardSymbol[1][4] = '8';
   keyboardSymbol[1][5] = '7';
-  keyboardSymbol[1][6] = '\0';
+  keyboardSymbol[1][6] = '\0';  // Left Shift
 
   keyboardSymbol[2][0] = '3';
   keyboardSymbol[2][1] = '/';
   keyboardSymbol[2][2] = '(';
-  keyboardSymbol[2][3] = '\0';
+  keyboardSymbol[2][3] = '\0';  // Right Shift
   keyboardSymbol[2][4] = '?';
   keyboardSymbol[2][5] = '9';
   keyboardSymbol[2][6] = '6';
@@ -102,7 +106,7 @@ void setup() {
   keyboardSymbol[3][0] = '_';
   keyboardSymbol[3][1] = ':';
   keyboardSymbol[3][2] = ')';
-  keyboardSymbol[3][3] = '\0';
+  keyboardSymbol[3][3] = (char)0x0a;  // Enter
   keyboardSymbol[3][4] = '!';
   keyboardSymbol[3][5] = ',';
   keyboardSymbol[3][6] = ';';
@@ -110,172 +114,195 @@ void setup() {
   keyboardSymbol[4][0] = '+';
   keyboardSymbol[4][1] = '"';
   keyboardSymbol[4][2] = '-';
-  keyboardSymbol[4][3] = '\0';
+  keyboardSymbol[4][3] = (char)0x7f;  // Delete
   keyboardSymbol[4][4] = '\0';
   keyboardSymbol[4][5] = '.';
   keyboardSymbol[4][6] = '\'';
 
-  symbolSelected = false;
-
   // Set PIN modes for keyboard matrix and backlight
   for (int x = 0; x < rowCount; x++) {
-    pinMode(rows[x], INPUT);
+    pinMode(rowPins[x], INPUT);
   }
   for (int x = 0; x < colCount; x++) {
-    pinMode(cols[x], INPUT_PULLUP);
+    pinMode(colPins[x], INPUT_PULLUP);
   }
-  pinMode(LITE, OUTPUT);
-  digitalWrite(LITE, backlightState);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, backlightState);
 
   // Start Serial and I2C communication
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Wire.onRequest(onRequest);
-  Wire.onReceive(onReceive);
-  Wire.begin((uint8_t)I2C_ADDR, SDA, SCL, 0);
+  Wire.onReceive(i2cReceive);
+  Wire.onRequest(i2cTransmit);
+  Wire.begin((uint8_t)I2C_ADDR, SDA_PIN, SCL_PIN, 0);
   Serial.print("Started I2C interface on address: ");
   Serial.println(I2C_ADDR);
 }
 
 void loop() {
-  readMatrix();
-  printMatrix();
+  readKeyMatrix();
+  processKeyStates();
+}
 
-  // Process keypresses
-  if (keyPressed(3, 3)) {
-    Serial.println("");
-    i2cBuffer.push((char)0x0D);
-  }
-  if (keyPressed(4, 3)) {
-    Serial.println("backspace");
-    i2cBuffer.push((char)0x08);
-  }
-  if (keyActive(0, 4) && keyPressed(3, 4)) {
-    Serial.println("Alt+B");
-    setBacklight(!backlightState);
-  }
-  if (keyActive(0, 4) && keyPressed(4, 1)) {
-    Serial.println("Alt+L");
-    i2cBuffer.push((char)0x0C);
+/**
+ * Receives and parses data from the host device.
+ */
+void i2cReceive(int bytesRead) {
+  if (0 < bytesRead) {
+    byte address = Wire.read();
+    Serial.printf("REC[%x]: %x -> ", bytesRead, address);
+
+    byte data[bytesRead - 1];
+    for (byte i = 0; i < (bytesRead - 1); i++) {
+      byte x = Wire.read();
+      Serial.print(x);
+      data[i] = x;
+    }
+    Serial.println();
+
+    // switch (address) {
+    //   case 0x0:
+    //     if (0 < sizeof(data)) {
+    //       eventsRequested = (uint8_t)data[0];
+    //     } else {
+    //       eventsRequested = 1;
+    //     }
+    //     break;
+    //   case 0x1:
+    //     key = eventBuffer.size();
+    //     break;
+    //   case 0x10:
+    //     setBacklight(!backlightState);
+    //     break;
+    //   case 0x11:
+    //     setBacklight(true);
+    //     break;
+    //   case 0x12:
+    //     setBacklight(false);
+    //     break;
+    //   default:
+    //     Serial.print("UNKNOWN: ");
+    //     Serial.println(address);
+    // }
   }
 }
 
-void onRequest() {
-  char key;
-  if (i2cBuffer.size() > 0) {
-    key = (char)i2cBuffer.pop();
-  } else {
-    key = '\0';
-  }
-  Wire.print(key);
-  Serial.print("REQUEST: ");
-  Serial.println(key);
-}
-
-void onReceive(int bytesRead) {
-  char cmd;
-  while (Wire.available()) {
-    cmd = Wire.read();
-    Serial.print("RECEIVE: ");
-    Serial.println(cmd);
-    switch (cmd) {
-      case 0x0:
-        // TODO: Implement
-        // i2cBuffer.flush();
-        break;
-      case 0x10:
-        setBacklight(false);
-        break;
-      case 0x11:
-        setBacklight(true);
-        break;
-      case 0x12:
-        setBacklight(!backlightState);
-        break;
-      default:
-        Serial.print("Unknown command: ");
-        Serial.println(cmd);
+/**
+ * Builds and transmits a response to the host device.
+ */
+void i2cTransmit() {
+  // TODO: Request multiple keypresses at one time.
+  byte response[eventsRequested];
+  for (byte i = 0; i < eventsRequested; i++) {
+    if (0 < eventBuffer.size()) {
+      response[i] = (byte)eventBuffer.front();
+      eventBuffer.pop();
+    } else {
+      response[i] = '\0';
     }
   }
+  Wire.write(response, sizeof(response));
+  eventsRequested = 1;  // Reset
 }
 
-void readMatrix() {
-  int delayTime = 0;
-  // iterate the columns
+/**
+ * Reads the keyboard pin states and stores them for processing.
+ */
+void readKeyMatrix() {
   for (int colIndex = 0; colIndex < colCount; colIndex++) {
-    // col: set to output to low
-    uint8_t curCol = cols[colIndex];
+    uint8_t curCol = colPins[colIndex];
     pinMode(curCol, OUTPUT);
     digitalWrite(curCol, LOW);
 
-    // row: iterate through the rows
     for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-      uint8_t rowCol = rows[rowIndex];
+      uint8_t rowCol = rowPins[rowIndex];
       pinMode(rowCol, INPUT_PULLUP);
-      delay(1);  // arduino is not fast enough to switch input/output modes
+      delay(1);  // Not fast enough to switch input/output modes
 
       bool buttonPressed = (digitalRead(rowCol) == LOW);
-
       keys[colIndex][rowIndex] = buttonPressed;
       if ((lastValue[colIndex][rowIndex] != buttonPressed)) {
         changedValue[colIndex][rowIndex] = true;
       } else {
         changedValue[colIndex][rowIndex] = false;
       }
-
       lastValue[colIndex][rowIndex] = buttonPressed;
       pinMode(rowCol, INPUT);
     }
-    // disable the column
     pinMode(curCol, INPUT);
-  }
-
-  // TODO: Move this
-  if (keyPressed(0, 2)) {
-    symbolSelected = !symbolSelected;
   }
 }
 
-void printMatrix() {
-  for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-    for (int colIndex = 0; colIndex < colCount; colIndex++) {
-      // we only want to print if the key is pressed and it is a printable
-      // character
-      if (keyPressed(colIndex, rowIndex) &&
-          isPrintableKey(colIndex, rowIndex)) {
-        char toPrint;
-        // Symbol key modifier
-        if (symbolSelected) {
-          toPrint = char(keyboardSymbol[colIndex][rowIndex]);
-          symbolSelected = false;
-          // Shift Key Modifier
-        } else if (keyActive(1, 6) || keyActive(2, 3)) {
-          toPrint = (char)((int)char(keyboard[colIndex][rowIndex]) - 32);
-        } else {
-          toPrint = char(keyboard[colIndex][rowIndex]);
-        }
+/**
+ * Processes the keyboard state and adds key events to the queue.
+ */
+void processKeyStates() {
+  // Hot keys
+  if (keyPressed(0, 4) && keyReleased(3, 4)) {
+    Serial.println("HOTKEY: Alt + B");
+    setBacklight(!backlightState);
+  } else if (keyPressed(0, 4) && keyReleased(4, 1)) {
+    Serial.println("HOTKEY: Alt + L");
+    addKeyEvent((char)0x0c);
 
-        // Add key to I2C buffer
-        Serial.print("KEY: ");
-        Serial.println(toPrint);
-        i2cBuffer.push(toPrint);
+    // Regular keys
+  } else {
+    for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+      for (int colIndex = 0; colIndex < colCount; colIndex++) {
+        if (keyReleased(colIndex, rowIndex) &&
+            isAssignedKey(colIndex, rowIndex)) {
+          char key;
+          // Symbol Modifier
+          if (keyPressed(0, 2)) {
+            addKeyEvent(keyboardSymbol[colIndex][rowIndex]);
+            // Shift Modifier
+          } else if ((keyPressed(1, 6) || keyPressed(2, 3)) &&
+                     isLowerCase((uint8_t)keyboard[colIndex][rowIndex])) {
+            addKeyEvent((char)((uint8_t)keyboard[colIndex][rowIndex] - 32));
+          } else {
+            addKeyEvent(keyboard[colIndex][rowIndex]);
+          }
+        }
       }
     }
   }
 }
 
-bool keyPressed(int colIndex, int rowIndex) {
+/**
+ * Returns true if the key was pressed and released.
+ */
+bool keyReleased(int colIndex, int rowIndex) {
   return changedValue[colIndex][rowIndex] && keys[colIndex][rowIndex];
 }
 
-bool keyActive(int colIndex, int rowIndex) { return keys[colIndex][rowIndex]; }
+/**
+ * Returns true if key was pressed but not released.
+ */
+bool keyPressed(int colIndex, int rowIndex) { return keys[colIndex][rowIndex]; }
 
-bool isPrintableKey(int colIndex, int rowIndex) {
+/**
+ * Checks if the key is a printing character.
+ */
+bool isAssignedKey(int colIndex, int rowIndex) {
   return keyboardSymbol[colIndex][rowIndex] != '\0' ||
          keyboard[colIndex][rowIndex] != '\0';
 }
 
+/**
+ * Adds a key character event to the buffer.
+ */
+void addKeyEvent(char key) {
+  Serial.print("EVENT: ");
+  Serial.println(key);
+  eventBuffer.push(key);
+}
+
+/**
+ * Turns the keyboard backlight on or off.
+ */
 void setBacklight(bool state) {
-  digitalWrite(LITE, state);
+  digitalWrite(LED_PIN, state);
   backlightState = state;
+  Serial.print("LIGHT: ");
+  Serial.println(state);
 }
